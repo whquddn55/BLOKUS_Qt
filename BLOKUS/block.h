04a955671f2b95e57264d5eb78cb878ffeab8622
whquddn55 <whquddn55@naver.com>
2017-06-21 13:07:09 +0900

    Implement .h files

--- BLOKUS/block.h
+++ BLOKUS/block.h
@@ -1,4 +1,28 @@
-#ifndef BLOCK_H
-#define BLOCK_H
+#pragma once
+#include "constants.h"
+class Block{
+private:
+    int cur[2];
+    int pos[5][2];
+    BLOCKCOLOR color;
+    int type;
+    int state;
+public:
+    Block(const int cur_x = 0, const int cur_y = 0, BLOCKCOLOR color = GRAY, const int type = 0);
+    Block(const Block& copyBlock);
 
-#endif // BLOCK_H
+    int getCurX() const;
+    int getCurY() const;
+    int getPos(const int index, bool isX) const;
+    int getType() const;
+    BLOCKCOLOR getColor() const;
+    int getState() const;
+    void setCur(const int x, const int y);
+    void setColor(const BLOCKCOLOR color);
+    void rotate(bool up);
+    void move(const DIRECTION direction);
+
+    void operator=(const Block& copy);
+private:
+    void setPos();
+};
--- BLOKUS/mainwindow.h
+++ BLOKUS/mainwindow.h
@@ -1,7 +1,12 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
+#pragma once
 #include <QMainWindow>
+#include <cstring>
+#include <QLabel>
+#include <QWheelEvent>
+#include "ui_mainwindow.h"
+#include "table.h"
+#include "user.h"
+#include "constants.h"
 
 namespace Ui {
 class MainWindow;
@@ -11,12 +16,29 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    Ui::MainWindow *ui;
+    Table gameTable;
+    User user[4];
+    QLabel* a_PixmapLabel[4][3][7]; // Data of TableWidge
+    QLabel* selected_PixmapLabel;
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
 private:
-    Ui::MainWindow *ui;
+    void deleteTableWidgetPix(const int index, int type);
+    void setSelectedTableWidgetPix(int index, int type);
+    void setTableWidget();
+    void wheelEvent(QWheelEvent *event);
+private slots:
+    void on_gameTableWidget_cellEntered(int row, int column);
+    void on_gameTableWidget_cellClicked(int row, int column);
+    void on_tableWidget_1_cellClicked(int row, int column);
+    void on_tableWidget_2_cellClicked(int row, int column);
+    void on_tableWidget_3_cellClicked(int row, int column);
+    void on_tableWidget_4_cellClicked(int row, int column);
+    void on_endButton_1_clicked(bool checked);
+    void on_endButton_2_clicked(bool checked);
+    void on_endButton_3_clicked(bool checked);
+    void on_endButton_4_clicked(bool checked);
 };
-
-#endif // MAINWINDOW_H
